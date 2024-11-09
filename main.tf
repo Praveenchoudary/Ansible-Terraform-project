ad81dd5533e8a98660bcb08bfd328c8aef9fbecf
PRAVEEN CHINTHALA <96223263+Praveenchoudary@users.noreply.github.com>
2024-11-09 20:47:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -30,6 +30,10 @@ resource "aws_autoscaling_group" "web_server_asg" {
     health_check_type    = "EC2"
     load_balancers       = [aws_elb.web_server_lb.name]
     availability_zones    = ["us-east-1a", "us-east-1b"] 
+    launch_template {
+        id      = aws_launch_template.web_server_as.id
+        version = "$Latest"
+      }
     
   }
 
